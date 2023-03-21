a2bec87a23386eae8e8538a6157c588ae6901753
julianC <mantillajulian234@gmail.com>
2023-03-20 21:04:08 -0500

    implemento listas añado integracion con bash

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
-import sys
+import sys, os
 
 
-clients = 'pablo,ricardo,oscar'
+clients = ['Pablo','Ricardo', 'Mark', 'Oscar']
 
 
 def search_client(client_name):
     global clients
-    client_list = clients.split(',')
 
-    for client in client_list:
+    for client in clients:
         if client_name != client:
             continue
         else:
@@ -20,21 +19,22 @@ def create_client(client_name):
     global clients
 
     if client_name not in clients:
-        clients += client_name
-        _add_comma()
+        clients.append(client_name)
     else:
         print('Client already in client\'s list')
 
 
 def list_clients():
-    print(clients)
+    for idx, client in enumerate(clients):
+        print('{}: {}'.format(idx+1, client))
 
 
 def update_client(client_name, updated_name):
     global clients
 
     if client_name in clients:
-        clients = clients.replace(client_name + ',', updated_name + ',')
+        idx = clients.index(client_name)
+        clients[idx] = updated_name 
     else:
         print('Client not in client\'s list')
 
@@ -43,21 +43,17 @@ def delete_client(client_name):
     global clients
 
     if client_name in clients:
-        clients = clients.replace(client_name + ',', '')
+        clients.remove(client_name)
+
     else:
         print('Client not in client\'s list')
 
-
-def _add_comma():
-    global clients
-
-    clients += ','
 
 def _get_client_name():
     client_name = None
 
     while not client_name:
-        client_name = input('What is the client name?: ')
+        client_name = input('What is the client name?: ').capitalize()
 
         if client_name == 'exit':
             client_name = None
@@ -71,6 +67,8 @@ def _get_client_name():
 
 def _print_welcome():
     print('WELCOME TO PLATZI VENTAS')
+    os.system('date +"Today is: %A %d %B"')
+
     print('*' * 50)
     print('What would you like to do today?:')
     print('[C]reate client')
@@ -91,7 +89,7 @@ if __name__ == '__main__':
 
     elif command == 'U':
         client_name = _get_client_name()
-        updated_name = input('What is name of the new client? ')
+        updated_name = input('What is name of the new client? ').capitalize()
         update_client(client_name, updated_name)
         list_clients()
 
@@ -107,8 +105,9 @@ if __name__ == '__main__':
         if found:
             print('{} is the client\'s list'.format(client_name))
         else:
-            print('{} is not in the client\'s list'.format(client_name))
+            os.system('echo NO MATCH! | lolcat')
             print(f' el valor de found: {found}')
 
     else:
         print('Invalid command')
+        
